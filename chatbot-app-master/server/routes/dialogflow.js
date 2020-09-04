de31ae128690bc62740abfad354f584b7e585ca6
jongeunShin95 <jongeunshin95@gmail.com>
2020-09-05 01:33:26 +0900

    Update 4/Event Route 만들기

--- chatbot-app-master/server/routes/dialogflow.js
+++ chatbot-app-master/server/routes/dialogflow.js
@@ -38,4 +38,28 @@ router.post('/textQuery', async (req, res) => {
       res.send(result);
 });
 
+router.post('/eventQuery', async (req, res) => {
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      event: {
+        // The query to send to the dialogflow agent
+        name: req.body.event,
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
+ 
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+
+  res.send(result);
+});
+
 module.exports = router;
