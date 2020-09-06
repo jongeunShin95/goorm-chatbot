625d513bef937b6b8ea13f42dfc7e2ea4367575e
jongeunShin95 <jongeunshin95@gmail.com>
2020-09-06 18:36:33 +0900

    Update 6/TextQuery Function 만들기

--- chatbot-app-master/client/src/Chatbot/Chatbot.js
+++ chatbot-app-master/client/src/Chatbot/Chatbot.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import Axios from 'axios';
 
 function Chatbot() {
+    const textQuery = async (text) => {
+        let conversation = {
+            who: 'user',
+            'content': {
+                text: {
+                    text: text
+                }
+            }
+        }
+        console.log(conversation);
+        const textQueryVariables = {
+            text
+        }
+        
+        try {
+            const res = await Axios.post('/api/dialogflow/textQuery', textQueryVariables);
+            const content = res.data.fulfillmentMessages[0];
+            conversation = {
+                who: 'bot',
+                content: content
+            }
+            console.log(conversation);
+        } catch(e) {
+            conversation = {
+                who: 'bot',
+                content: {
+                    text: {
+                        text: "Error just occured, please check the problem"
+                    }
+                }
+            }
+            console.log(conversation);
+        }
+
+    }
     const keyPressHandler = (e) => {
         if (e.key === "Enter") {
             if (!e.target.value) {
                 return alert('you need to type something first');
             }
+
+            textQuery(e.target.value);
 
             e.target.value = "";
         }
@@ -27,7 +65,7 @@ function Chatbot() {
                 placeholder="Send a message..."
                 onKeyPress={keyPressHandler}
                 type="text"
-            />
+            /> 
         </div>
     )
 }
